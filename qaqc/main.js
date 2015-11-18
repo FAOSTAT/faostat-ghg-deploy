9397403bdaf01a8a2f59e0cddf705befc58fde88
simonefenix <simone.murzilli@fao.org>
2015-11-18 18:06:28 +0100

    .

---
+++ qaqc/main.js
@@ -0,0 +1,80 @@
+
+var CDN = '//fenixrepo.fao.org/cdn/';
+var projectRoot = '//localhost:20000/';
+
+
+require.config({
+
+    waitSeconds: 20,
+
+    text: {
+        useXhr: function (url, protocol, hostname, port) {
+            return true;
+        }
+    },
+
+    paths: {
+
+        'domReady': CDN + '/js/requirejs/plugins/domready/2.0.1/domReady',
+        'i18n':  CDN + '/js/requirejs/plugins/i18n/2.0.4/i18n',
+        'text':  CDN + '/js/requirejs/plugins/text/2.0.12/text',
+
+        'jquery':  CDN + 'js/jquery/1.10.2/jquery-1.10.2.min',
+        'highcharts': CDN + 'js/highcharts/4.0.4/js/highcharts',
+        'highcharts-exporting': CDN + 'js/highcharts/4.0.4/js/modules/exporting',
+        'f3-ghg-chart': CDN + 'faostat3/f3-ghg-chart/f3-ghg-chart',
+        'wide-table': CDN + 'faostat3/wide-table/wide-table-min',
+        'handlebars': CDN + 'js/handlebars/2.0.0/handlebars',
+        'chosen': CDN + 'js/chosen/1.2.0/chosen.jquery.min',
+        'jshashtable': CDN + 'js/jshashtable/0.0.1/jshashtable',
+
+        'amplify': CDN + '/js/amplify/1.1.2/amplify.min',
+        'underscore': CDN + "/js/underscore/1.7.0/underscore.min",
+
+        FAOSTAT_UI_COMMONS: projectRoot + 'faostat-ui-commons/js/faostat-ui-commons',
+        faostat_ui_commons : projectRoot + 'faostat-ui-commons',
+        FAOSTAT_UI_WIDE_TABLES: projectRoot + 'faostat-ui-wide-tables/js/faostat-ui-wide-tables',
+        faostat_ui_wide_tables: projectRoot + 'faostat-ui-wide-tables',
+        FAOSTAT_UI_ANALYSIS_GHG_QA_QC: projectRoot + 'faostat-ui-analysis-ghg-qaqc/js/faostat-ui-analysis-ghg-qaqc',
+        faostat_ui_analysis_ghg_qa_qc: projectRoot + 'faostat-ui-analysis-ghg-qaqc',
+        FAOSTAT_UI_ANALYSIS_GHG_QAQC_PDF: projectRoot + 'faostat-ui-analysis-ghg-qaqc/pdf/',
+
+        'bootstrap': CDN + 'js/bootstrap/3.2/js/bootstrap.min',
+        'sweetAlert': CDN + 'js/sweet-alert/0.4.2/sweet-alert'
+
+
+    },
+
+    shim: {
+
+        'highcharts': ['jquery'],
+        'highcharts-exporting': ['highcharts'],
+        'f3-ghg-chart': ['highcharts'],
+        'chosen': ['jquery'],
+        'handlebars': ['jquery'],
+        'amplify': ['jquery'],
+        'bootstrap': ['jquery']
+
+    }
+});
+
+var locale = "E"; //S, F
+
+require.config({'locale': locale});
+
+/* Bootstrap the application. */
+require([
+    'FAOSTAT_UI_ANALYSIS_GHG_QA_QC',
+    'domReady!'
+], function (MODULE) {
+
+    var module = new MODULE();
+
+    module.init({
+        placeholder_id: 'fs-qaqc-module',
+        //lang: 'fr'
+    })
+
+
+
+});
